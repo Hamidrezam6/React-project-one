8239706e289bb6689ecc9ef3305584358cb096b9
hamidreza <6ghanbari.hamid6@gmail.com>
2025-12-28 00:08:30 -0800

    click event added

--- react-sample-vite/src/components/list.jsx
+++ react-sample-vite/src/components/list.jsx
@@ -1,13 +1,28 @@
 const List = () => {
     const items = ['apple', 'samsung', 'tesla', 'benz'];
     return (
-        <ul>
-            {items.map((item) => {
-                return <li>{item}</li>
-            })}
-        </ul>
-        
-    )
+        <>
+            <label>
+                 Best High Tech Companies
+            </label>
+            <ul>
+                {items.length ? (
+                    items.map((item, index) => (
+                        <li 
+                          onClick={(e) => {
+                            console.log ('clicked', e);
+                          }}
+                          key={item}
+                        >
+                          {item}---{index}
+                        </li>
+                    ))
+                ) : (
+                    <div> There is no item ...</div>
+                )}
+            </ul>
+        </>
+    );
 };
 
 export default List;
